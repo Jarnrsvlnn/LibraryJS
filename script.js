9e8960f9cd0265500779b40004b7f45c5636fcdb
Jarnrsvlnn <173128640+jarnrsvlnn@users.noreply.github.com>
2025-08-03 10:01:18 +0800

    Added a function where when you click the add button it adds the book into the array and then displays the book infos

---
+++ script.js
@@ -0,0 +1,41 @@
+const createBook = document.querySelector(".new-book");
+
+const myLibrary = [ // this is where all the book objects will be stored in
+
+];
+
+function Book(title, author, pages, readState) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.readState = readState;
+    this.id = crypto.randomUUID();
+}
+
+function addBookToLibrary(title, author, pages, readState) { // every book that gets created will be handled by this and saved to the library array
+    const newBook = new Book(title, author, pages, readState);
+    myLibrary.push(newBook);
+}
+
+function displayBook() {
+    const bookContainer = document.querySelector(".book-container");
+    bookContainer.innerHTML = '';
+
+    for (let book of myLibrary) {
+        const card = document.createElement('div');
+        card.classList.add("book-card");
+        card.innerHTML = `
+        <h1>${book.title}</h1>
+        <h2>${book.author}</h2>
+        <p>${book.pages}</p>
+        <p>${book.readState ? 'Read' : 'Not Read'}</p>
+        `;
+        bookContainer.append(card);
+    }
+}
+
+createBook.addEventListener('click', () => {
+    addBookToLibrary('Harry Potter', 'J.K Rowling', 300, true)
+    displayBook();
+});
+
