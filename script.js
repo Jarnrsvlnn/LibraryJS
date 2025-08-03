8e945c0cc4ec6069142db48765da7255b0ba61a3
Jarnrsvlnn <173128640+jarnrsvlnn@users.noreply.github.com>
2025-08-03 16:41:53 +0800

    Completed all the required function for the library to work, with no CSS still

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const createBook = document.querySelector(".create-book");
-const addBook = document.querySelector(".addBook")
+const addBook = document.querySelector("form")
 const showForm = document.querySelector("dialog");
 const closeForm = document.querySelector(".close-book-form");
 
@@ -11,8 +11,12 @@ function Book(title, author, pages, readState) {
     this.title = title;
     this.author = author;
     this.pages = pages;
-    this.readState = readState;
+    this.readState = readState === 'true';
     this.id = crypto.randomUUID();
+}
+
+Book.prototype.changeReadStatus = function() {
+    this.readState = !this.readState;
 }
 
 function addBookToLibrary(title, author, pages, readState) { // every book that gets created will be handled by this and saved to the library array
@@ -26,14 +30,41 @@ function displayBook() {
 
     for (let book of myLibrary) {
         const card = document.createElement('div');
+        const deleteBook = document.createElement('button');
+        const toggleReadStatus = document.createElement('button');
+        toggleReadStatus.textContent = 'Change Status'
+        deleteBook.textContent = "Remove";
+        deleteBook.dataset.id = book.id;
+
+        toggleReadStatus.addEventListener('click', () => {
+            book.changeReadStatus();
+            displayBook();
+        })
+
+        deleteBook.addEventListener('click', (e) => {
+            const bookID = e.target.dataset.id;
+            const bookIndex = myLibrary.findIndex(book => 
+                {
+                    return book.id == bookID
+                });
+
+            if (bookIndex !== -1) {
+                myLibrary.splice(bookIndex, 1)
+                displayBook();
+            }
+        })
+
         card.classList.add("book-card");
         card.innerHTML = `
         <h1>${book.title}</h1>
         <h2>${book.author}</h2>
         <p>${book.pages}</p>
         <p>${book.readState ? 'Read' : 'Not Read'}</p>
-        `;
+        `
+        
         bookContainer.append(card);
+        card.append(toggleReadStatus);
+        card.append(deleteBook);
     }
 }
 
@@ -45,4 +76,18 @@ closeForm.addEventListener('click', () => {
     showForm.close();
 });
 
+addBook.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const title = document.getElementById("book-title").value;
+    const author = document.getElementById("book-author").value;
+    const page = document.getElementById("book-pages").value;
+    const hasRead = document.querySelector("input[name='readState']:checked")?.value;
+
+    addBookToLibrary(title, author, page, hasRead);
+    displayBook();
+    console.log('its working');
+    showForm.close();
+    addBook.reset();
+});
 
